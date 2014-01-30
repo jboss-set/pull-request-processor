59edf0ceaf6f50476678d612117b5a4588270b56
Ivo Studensky <istudens@redhat.com>
2014-01-31 00:02:38 +0100

    updates for new evaluator facility

--- src/main/java/org/jboss/pull/processor/Processor.java
+++ src/main/java/org/jboss/pull/processor/Processor.java
@@ -28,8 +28,10 @@ import java.net.URLEncoder;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.regex.Pattern;
 
@@ -38,6 +40,7 @@ import org.eclipse.egit.github.core.PullRequest;
 import org.jboss.pull.shared.Bug;
 import org.jboss.pull.shared.PullHelper;
 import org.jboss.pull.shared.Util;
+import org.jboss.pull.shared.spi.PullEvaluator;
 
 
 /**
@@ -60,43 +63,50 @@ public class Processor {
     /** how many PRs can be merged in one batch */
     private static final int MERGE_BATCH_LIMIT = 20;
 
-    private String BASE_HOST;
-    private String BASE_PORT;
-    private String BASE_URI;
+    private final String BASE_HOST;
+    private final String BASE_PORT;
+    private final String BASE_URI;
 
-    private String BASE_URL;
-    private String BASE_JOB_URL;
-    private String PUBLISH_JOB_URL;
-    private String JENKINS_JOB_TOKEN;
+    private final String BASE_URL;
+    private final String BASE_JOB_URL;
+    private final String PUBLISH_JOB_URL;
+    private final String JENKINS_JOB_TOKEN;
 
-    private String JENKINS_JOB_NAME;
-    private String COMMENT_PRIVATE_LINK;
+    private final String JENKINS_JOB_NAME;
+    private final String COMMENT_PRIVATE_LINK;
 
-    private boolean DRY_RUN;
+    private final boolean DRY_RUN;
 
-    private PullHelper helper;
+    private final String targetBranch;
+    private final PullHelper helper;
 
-    public static void main(String[] args) throws Exception {
-        try {
-            Processor processor = new Processor();
-            processor.run();
-            System.exit(0);
-        } catch (Exception e) {
-            System.err.println(e);
-            e.printStackTrace(System.err);
+    public static void main(String[] argv) throws Exception {
+        if (argv.length == 2) {
+            try {
+                Processor processor = new Processor(argv[0], argv[1]);
+                processor.run();
+                System.exit(0);
+            } catch (Exception e) {
+                System.err.println(e);
+                e.printStackTrace(System.err);
+            }
+        } else {
+            System.err.println(usage());
         }
         System.exit(1);
     }
 
 
-    public Processor() throws Exception {
+    public Processor(final String targetBranchProperty, final String jenkinsJobNameProperty) throws Exception {
         helper = new PullHelper("processor.properties.file", "./processor.properties");
+
+        targetBranch = Util.require(helper.getProps(), targetBranchProperty);
+        JENKINS_JOB_NAME = Util.require(helper.getProps(), jenkinsJobNameProperty);
 
         BASE_HOST = Util.require(helper.getProps(), "jenkins.host");
         BASE_PORT = Util.require(helper.getProps(), "jenkins.port");
         BASE_URI = Util.get(helper.getProps(), "jenkins.uri", "");
         PUBLISH_JOB_URL = Util.require(helper.getProps(), "jenkins.publish.url");
-        JENKINS_JOB_NAME = Util.require(helper.getProps(), "jenkins.job.name");
         JENKINS_JOB_TOKEN = Util.get(helper.getProps(), "jenkins.job.token");
         BASE_URL = "http://" + BASE_HOST + ":" + BASE_PORT + BASE_URI;
         BASE_JOB_URL = BASE_URL + "/job";
@@ -127,7 +137,7 @@ public class Processor {
             final Set<PullRequest> pullsToMerge = new LinkedHashSet<PullRequest>();
             final Set<PullRequest> pullsPending = new LinkedHashSet<PullRequest>();
             final Set<PullRequest> pullsRunning = new LinkedHashSet<PullRequest>();
-            final Set<PullRequest> pullsToComplain = new LinkedHashSet<PullRequest>();
+            final Map<PullRequest, List<String>> pullsToComplain = new LinkedHashMap<PullRequest, List<String>>();
 
             for (PullRequest pullRequest : pullRequests) {
                 if (pullRequest.getHead().getSha() == null) {
@@ -135,7 +145,7 @@ public class Processor {
                     continue;
                 }
 
-                if (! helper.getGithubBranch().equals(pullRequest.getBase().getRef())) {
+                if (! targetBranch.equals(pullRequest.getBase().getRef())) {
                     continue;
                 }
 
@@ -189,10 +199,11 @@ public class Processor {
                     pullsRunning.add(pullRequest);
                 } else if (trigger) {
                     // check other conditions, i.e. upstream pull request and bugzilla
-                    if (helper.isMergeable(pullRequest)) {
+                    final PullEvaluator.Result mergeable = helper.isMergeable(pullRequest);
+                    if (mergeable.isMergeable()) {
                         pullsToMerge.add(pullRequest);
                     } else {
-                        pullsToComplain.add(pullRequest);
+                        pullsToComplain.put(pullRequest, mergeable.getDescription());
                     }
                 } else {
                     Comment lastComment = comments.get(comments.size() - 1);
@@ -206,19 +217,19 @@ public class Processor {
 
             // check the pending pulls and eventually update their state if they have been already started on Hudson
             for (PullRequest pendingPull : pullsPending) {
-                JenkinsBuild build = JenkinsBuild.findBuild(BASE_URL, JENKINS_JOB_NAME, Util.map("sha1", pendingPull.getHead().getSha(), "branch", helper.getGithubBranch()));
+                JenkinsBuild build = JenkinsBuild.findBuild(BASE_URL, JENKINS_JOB_NAME, Util.map("sha1", pendingPull.getHead().getSha(), "branch", pendingPull.getBase().getRef()));
                 if (build != null) {
                     // build in progress
-                    notifyBuildRunning(pendingPull, helper.getGithubBranch(), build.getBuild());
+                    notifyBuildRunning(pendingPull, build.getBuild());
                 }
             }
 
             // check the running pulls and eventually update their state according to the result of the Hudson build if it is finished
             for (PullRequest runningPull : pullsRunning) {
-                JenkinsBuild build = JenkinsBuild.findBuild(BASE_URL, JENKINS_JOB_NAME, Util.map("sha1", runningPull.getHead().getSha(), "branch", helper.getGithubBranch()));
+                JenkinsBuild build = JenkinsBuild.findBuild(BASE_URL, JENKINS_JOB_NAME, Util.map("sha1", runningPull.getHead().getSha(), "branch", runningPull.getBase().getRef()));
                 if (build != null && build.getStatus() != null) {
                     // build finished
-                    notifyBuildCompleted(runningPull, helper.getGithubBranch(), build.getBuild(), build.getStatus());
+                    notifyBuildCompleted(runningPull, build.getBuild(), build.getStatus());
                 }
             }
 
@@ -231,8 +242,8 @@ public class Processor {
             }
 
             // complain about PRs which don't follow the rules
-            for (PullRequest pullToComplain : pullsToComplain) {
-                complain(pullToComplain);
+            for (Map.Entry<PullRequest, List<String>> pullToComplain : pullsToComplain.entrySet()) {
+                complain(pullToComplain.getKey(), pullToComplain.getValue());
             }
 
         } finally {
@@ -240,8 +251,8 @@ public class Processor {
         }
     }
 
-    private void notifyBuildCompleted(PullRequest pull, String branch, int buildNumber, String status) {
-        String comment = "Build " + buildNumber + " merging " + pull.getHead().getSha() + " to branch " + branch + " has been finished with outcome " + status + ":\n";
+    private void notifyBuildCompleted(PullRequest pull, int buildNumber, String status) {
+        String comment = "Build " + buildNumber + " merging " + pull.getHead().getSha() + " to branch " + pull.getBase().getRef() + " has been finished with outcome " + status + ":\n";
         comment += COMMENT_PRIVATE_LINK + buildNumber + "\n";
 
         String githubStatus = convertJenkinsStatus(status);
@@ -278,16 +289,16 @@ public class Processor {
         return "error";
     }
 
-    private void notifyBuildRunning(PullRequest pull, String branch, int buildNumber) {
-        String comment = "Build " + buildNumber + " merging " + pull.getHead().getSha() + " to branch " + branch + " has been started:\n";
+    private void notifyBuildRunning(PullRequest pull, int buildNumber) {
+        String comment = "Build " + buildNumber + " merging " + pull.getHead().getSha() + " to branch " + pull.getBase().getRef() + " has been started:\n";
         comment += COMMENT_PRIVATE_LINK + buildNumber + "\n";
 
         postComment(pull, comment);
         postStatus(pull, buildNumber, "pending");
     }
 
-    private void notifyBuildTriggered(PullRequest pull, String branch) {
-        String comment = "Build merging " + pull.getHead().getSha() + " to branch " + branch +  " has been triggered:\n";
+    private void notifyBuildTriggered(PullRequest pull) {
+        String comment = "Build merging " + pull.getHead().getSha() + " to branch " + pull.getBase().getRef() +  " has been triggered:\n";
         comment += COMMENT_PRIVATE_LINK +"\n";
 
         postComment(pull, comment);
@@ -303,17 +314,21 @@ public class Processor {
             for (PullRequest pull : pullsToMerge) {
                 sha1s.append(delim).append(pull.getHead().getSha());
                 pulls.append(delim).append(Integer.toString(pull.getNumber()));
+                if (! targetBranch.equals(pull.getBase().getRef())) {
+                    // this should never happen
+                    throw new IllegalStateException("Base branch of a pull request is different to the configured target branch");
+                }
                 delim = " ";
                 if (++count > MERGE_BATCH_LIMIT) {
                     break;
                 }
             }
-            URL url = new URL(BASE_JOB_URL + "/" + JENKINS_JOB_NAME + "/buildWithParameters?token=" + JENKINS_JOB_TOKEN + "&pull=" + URLEncoder.encode(pulls.toString(), "UTF-8") +"&sha1=" + URLEncoder.encode(sha1s.toString(), "UTF-8") + "&branch=" + URLEncoder.encode(helper.getGithubBranch(), "UTF-8") + "&dryrun=" + DRY_RUN);
+            URL url = new URL(BASE_JOB_URL + "/" + JENKINS_JOB_NAME + "/buildWithParameters?token=" + JENKINS_JOB_TOKEN + "&pull=" + URLEncoder.encode(pulls.toString(), "UTF-8") +"&sha1=" + URLEncoder.encode(sha1s.toString(), "UTF-8") + "&branch=" + URLEncoder.encode(targetBranch, "UTF-8") + "&dryrun=" + DRY_RUN);
             urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.connect();
             if (urlConnection.getResponseCode() == 200) {
                 for (PullRequest pull : pullsToMerge) {
-                    notifyBuildTriggered(pull, helper.getGithubBranch());
+                    notifyBuildTriggered(pull);
                 }
             } else {
                 System.err.println("Problem triggering build for pulls: " + pullsToMerge + " response code: " + urlConnection.getResponseCode() + " - " + urlConnection.getResponseMessage());
@@ -329,18 +344,20 @@ public class Processor {
         }
     }
 
-    private void complain(PullRequest pull) {
-        String comment = "This PR cannot be merged due to non-compliance with the rules of the relevant EAP version. Please fix/complete its description first.";
+    private void complain(PullRequest pull, List<String> description) {
+        StringBuilder comment = new StringBuilder("This PR cannot be merged due to non-compliance with the rules of the relevant EAP version.\n");
+        comment.append("details:\n");
+        for (String detailDesc: description) {
+            comment.append(detailDesc).append("\n");
+        }
 
         try {
             final List<Comment> comments = helper.getIssueService().getComments(helper.getRepository(), pull.getNumber());
             Comment lastComment = comments.get(comments.size() - 1);
 
             if (lastComment.getBody().indexOf("cannot be merged due to non-compliance with the rules") == -1) {
-                if (! DRY_RUN) {
-                    postComment(pull, comment);
-//                    postStatus(pull, -1, "failure");
-                }
+                postComment(pull, comment.toString());
+//                postStatus(pull, -1, "failure");
             }
         } catch (IOException e) {
             System.err.printf("Could not get comments for pull %d due to %s\n", pull.getNumber(), e.getMessage());
@@ -374,4 +391,12 @@ public class Processor {
         return dateFormat.format(date);
     }
 
+    private static String usage() {
+        StringBuilder usage = new StringBuilder();
+        usage.append("java -jar pull-processor-1.0-SNAPSHOT.jar <property name of the target branch on github> <property name of dedicated jenkins merge job>\n\n");
+        usage.append("optional system properties:\n");
+        usage.append("-Dprocessor.properties.file defaults to \"./processor.properties\"\n");
+        usage.append("-Ddryrun=true to run without changing anything, i.e. simulated run, defaults to false\n");
+        return usage.toString();
+    }
 }
