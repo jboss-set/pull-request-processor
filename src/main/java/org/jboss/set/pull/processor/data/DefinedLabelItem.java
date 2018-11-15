71903c679f6e177db23573aeb9402c472c59ba8c
Chao Wang <chaowan@redhat.com>
2018-11-15 17:18:25 +0800

    [ISSUE-78] Merge PullRequestVsStreamComponentLabelEvaluator and UpstreamPullRequestLabelEvaluator and rename two DefinedLabelItem

--- src/main/java/org/jboss/set/pull/processor/data/DefinedLabelItem.java
+++ src/main/java/org/jboss/set/pull/processor/data/DefinedLabelItem.java
@@ -28,8 +28,8 @@ public class DefinedLabelItem extends LabelItem<DefinedLabelItem.LabelContent> {
         Missing_issue("Missing issue"),
         Missing_upstream_PR("Missing upstream PR"),
         Corrupted_upgrade_meta("Corrupted upgrade"),
-        Component_Repository_Mismatch("Repository Mismatch"),
-        Component_Branch_Mismatch("Branch Mismatch");
+        Upstream_PR_Repository_Mismatch("Upstream PR repository mismatched"),
+        Upstream_PR_Branch_Mismatch("Upstream PR branch mismatched");
 
         private String label;
 
